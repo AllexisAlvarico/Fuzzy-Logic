dfb511ac53bc0f210d3d66e4b837e99be519e8fc
AllexisAlvarico <c00242855@itcarlow.ie>
2022-01-19 19:34:58 +0000

    getting sfml to work

--- Fuzzy-Logic/Game.cpp
+++ Fuzzy-Logic/Game.cpp
@@ -1,15 +1,47 @@
 #include "Game.h"
 
-Game::Game()
+Game::Game() : m_window(sf::VideoMode(800u, 600u), "Fuzzy Logic")
 {
+	m_exitGame = false;
 }
 
 void Game::run()
 {
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	sf::Time timePerFrame = sf::seconds(1.f / 60.f);
+
+	while (m_window.isOpen())
+	{
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			update(timePerFrame);
+		}
+		render();
+	}
 }
 
 void Game::processEvents()
 {
+	sf::Event event;
+
+	while (m_window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+		{
+			m_exitGame = true;
+		}
+
+		if (event.type == sf::Keyboard::Escape)
+		{
+			m_exitGame = true;
+		}
+
+		if (m_exitGame)
+			m_window.close();
+	}
 }
 
 void Game::update(sf::Time t_deltaTime)
@@ -18,4 +50,6 @@ void Game::update(sf::Time t_deltaTime)
 
 void Game::render()
 {
+	m_window.clear(sf::Color::Black);
+	m_window.display();
 }
